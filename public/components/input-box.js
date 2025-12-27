db07fc950d3a8b2c39371cca55fc0ca3fe8596ff
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-12-27 17:07:27 -0300

    Add event to copy easily content from input boxes

--- public/components/input-box.js
+++ public/components/input-box.js
@@ -15,14 +15,14 @@ const inputBox = () =>
 	const TEXT_AREA = document.createElement("textarea");
 
 	const SELECT = document.createElement("select");
-	const BUTTON = document.createElement("button");
+	const COPIER = document.createElement("button");
 
 	CONTAINER.className = "converter-input-container";
 	NAV.className       = "converter-input-nav";
 	TEXT_AREA.className = "converter-input";
 
 	SELECT.className = "converter-input-type";
-	BUTTON.className = "i-copy converter-output-copier";
+	COPIER.className = "i-copy converter-output-copier";
 
 	let option;
 	for(const TEXT_CONTENT of CONVERTER_INPUT_TYPES)
@@ -33,11 +33,40 @@ const inputBox = () =>
 		SELECT.appendChild( option );
 	}
 
+	COPIER.addEventListener("click", () =>
+	{
+		if(navigator.clipboard !== undefined && navigator.clipboard.writeText !== undefined)
+		{
+			navigator.clipboard.writeText( TEXT_AREA.value )
+				.then(() =>
+				{
+					// copy success notification
+				})
+				.catch((ex) =>
+				{
+					console.error(ex);
+				});
+			return;
+		}
+
+		TEXT_AREA.focus();
+		TEXT_AREA.select();
+		TEXT_AREA.setSelectionRange(0, TEXT_AREA.value.length);
+
+		if(document.execCommand !== undefined && document.execCommand("copy"))
+		{
+			// copy success notification
+			return;
+		}
+
+		console.error(new InternalError("Impossible copy!"));
+	})
+
 	CONTAINER.appendChild( NAV );
 	CONTAINER.appendChild( TEXT_AREA );
 
 	NAV.appendChild( SELECT );
-	NAV.appendChild( BUTTON );
+	NAV.appendChild( COPIER );
 
 	CONVERTER.appendChild( CONTAINER );
 };
