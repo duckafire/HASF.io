ad806d467c999ae66961f8bd564a72ab660ae761
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-12-28 17:16:51 -0300

    Rework TBoxes:
    1 - Remove unnecessary tag (from `<template>`).
    2 - Add `<select`> ones.
    3 - Define default type of TBoxes ("output").
    4 - Add events to TBox buttons.
    5 - Change TBoxesContainer organization.
    6 - Change TBoxes dimensions.
    7 - Allow resize TBoxes height.
    8 - Increment TBOXES (global constant from JS).
    9 - Fix bugs from TBOXES methods.
    10 - Add function to apply events to TBoxes buttons.
    11 - Add treatment to TBoxes of type "input" (only 1st).
    12 - Remove delete button from first TBox ("input").

--- public/templates/tbox.js
+++ public/templates/tbox.js
@@ -10,15 +10,28 @@ const TBOXES = Object.freeze(new (class
 		this.__items__ = [];
 	}
 
+	__updateIds__(id, max = this.__items__.length)
+	{
+		for(let i = id; i < max; i++)
+			this.__items__[i].dataset.listId = i;
+	}
+
 	push(item)
 	{
-		this.__items__.push(item);
 		item.dataset.listId = this.__items__.length;
+		this.__items__.push(item);
+	}
+
+	insert(item, id)
+	{
+		item.dataset.listId = id;
+		this.__items__.splice(id, 0, item);
+		this.__updateIds__(id);
 	}
 
 	get(id)
 	{
-		return this.__items__[id];
+		return this.__items__[ parseInt(id) ];
 	}
 
 	len()
@@ -33,17 +46,101 @@ const TBOXES = Object.freeze(new (class
 
 		if(id === MAX)
 		{
-			this.__items__[id] = undefined;
+			this.__items__.pop();
 			return;
 		}
 
-		for(let i = id; i < MAX; i++)
-		{
-			this.__items__[i] = this.__items__[i + 1];
-			this.__items__[i].dataset.listId = i;
-		}
+		this.__items__.splice(id, 1);
+		this.__updateIds__(id, MAX);
 	}
 }));
+
+const __evCloneTBox__ = (comp) =>
+{
+	const ID = parseInt(comp.dataset.listId);
+	const CLONE = TBOXES.get( ID ).cloneNode(true);
+	const POSITION_REFERENCE = (
+		ID !== TBOXES.len() - 1
+			? TBOXES.get( ID + 1 )
+			: null
+	);
+
+	if(ID === 0)
+	{
+		// type:input -> type:output
+		const BTN_DEL = document.createElement("button");
+		const INPUT   = CLONE.querySelector(".tbox-input");
+
+		BTN_DEL.className = "tbox-btn-delete tbox-btn";
+		BTN_DEL.innerHTML = '<span class="i-trash"></span>';
+
+		INPUT.setAttribute("readonly", true);
+		INPUT.placeholder = "Result from past cell.";
+
+		CLONE.dataset.type = "output";
+		CLONE.querySelector(".tbox-nav").appendChild( BTN_DEL );
+	}
+
+	__applyTBoxEvents__(CLONE);
+
+	if(POSITION_REFERENCE !== null)
+	{
+		TBOXES.insert(CLONE, ID + 1);
+		TBOXES_CONTAINER.insertBefore(CLONE, POSITION_REFERENCE);
+		return;
+	}
+
+	TBOXES.push(CLONE);
+	TBOXES_CONTAINER.appendChild(CLONE);
+};
+
+const __evCopyFallbackTBox__ = (input) =>
+{
+	INPUT_AREA.focus();
+	INPUT_AREA.select();
+	INPUT_AREA.setSelectionRange(0, INPUT_AREA.value.length);
+
+	if(document.execCommand === undefined || !document.execCommand("copy"))
+		return; // failure notification
+
+	// success notification
+};
+
+const __evCopyTBox__ = (comp, inputArea) =>
+{
+	// TODO: apply notifications
+	if(navigator.clipboard === undefined || navigator.clipboard.writeText === undefined)
+		return __evCopyFallbackTBox__(inputArea);
+
+	navigator.clipboard.writeText( inputArea.value )
+		.then(() =>
+		{
+			// success notification
+		})
+		.catch(()=>
+		{
+			__evCopyFallbackTBox__(inputArea);
+		});
+};
+
+const __evDeleteTBox__ = (comp) =>
+{
+	// TODO: confirm action
+	TBOXES.rm( comp.dataset.listId );
+	TBOXES_CONTAINER.removeChild( comp );
+};
+
+const __applyTBoxEvents__ = (comp) =>
+{
+	const INPUT_AREA = comp.querySelector(".tbox-input");
+	const BTN_DEL    = comp.querySelector(".tbox-btn-delete");
+
+	comp.querySelector(".tbox-btn-clone").addEventListener("click", () => __evCloneTBox__(comp));
+	comp.querySelector(".tbox-btn-copy").addEventListener("click", () => __evCopyTBox__(comp, INPUT_AREA));
+
+	if(BTN_DEL !== null)
+		BTN_DEL.addEventListener("click", () => __evDeleteTBox__(comp));
+};
 
 const newTBox = () =>
 {
@@ -52,16 +149,15 @@ const newTBox = () =>
 	if(TBOXES.len() === 0)
 	{
 		TBOX.dataset.type = "input";
-		TBOX.querySelector(".tbox-input").placeholder = "Write here.";
-	}
-	else
-	{
-		TBOX.dataset.type = "output";
+		TBOX.querySelector(".tbox-nav").removeChild( TBOX.querySelector(".tbox-btn-delete") );
+
 		const INPUT = TBOX.querySelector(".tbox-input");
-		INPUT.placeholder = "Result from past cell.";
-		INPUT.readOnly = "readonly";
+
+		INPUT.placeholder = "Write here.";
+		INPUT.removeAttribute("readOnly");
 	}
 
+	__applyTBoxEvents__(TBOX);
 	TBOXES.push(TBOX);
 	TBOXES_CONTAINER.appendChild(TBOX);
 };
