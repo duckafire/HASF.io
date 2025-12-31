507b3127550bf2dec1244bbf313af0eebf90538d
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-12-31 17:54:15 -0300

    Add converter algorithm

--- public/events/generic.js
+++ public/events/generic.js
@@ -31,32 +31,6 @@ document.getElementById("copy-all-content").addEventListener("click", () =>
 	copyTextFromInputElem(result);
 });
 
-document.getElementById("converter-runner").addEventListener("click", () =>
-{
-	const MAX = TBOXES.len() - 1;
-	const INPUT_CLASS = ".tbox-input";
-	let cur, input, output;
+document.getElementById("converter-runner").addEventListener("click", CORE);
 
-	for(let i = 0; i < MAX; i++)
-	{
-		cur = TBOXES.get(i);
-		input  = cur.querySelector( INPUT_CLASS );
-		output = TBOXES.get( i + 1 ).querySelector( INPUT_CLASS );
-
-		output.value = input.value; // TMP
-
-		/* TODO
-		switch( cur.querySelector(".converter-input-type").value )
-		{
-			case "0": break;
-			case "1": break;
-			case "2": break;
-			case "3": break;
-		}*/
-	}
-});
-
-document.getElementById("create-input-box").addEventListener("click", () =>
-{
-	newTBox();
-});
+document.getElementById("create-input-box").addEventListener("click", newTBox);
