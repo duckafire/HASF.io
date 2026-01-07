edec34cec921a71b9c2a15c354ce4b5c2bb9c972
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-01-07 14:21:35 -0300

    Use JS DOM event "animationend" to remove notification cards, instead JS

--- public/templates/notif/dom.js
+++ public/templates/notif/dom.js
@@ -1,9 +1,8 @@
 const __GLOBAL_NOTIF_CONTAINER__ = document.getElementById("global-notif-container");
-let __notifTimeOfLive__ = null;
 
-const newNotif = (msg, destine) =>
+const newNotif = (msg, destine = 0) =>
 {
-	if(destine)
+	if(destine !== 0)
 	{
 		console.warn( new Error(msg) );
 
@@ -16,20 +15,10 @@ const newNotif = (msg, destine) =>
 	NOTIF.className   = "global-notif-item";
 	NOTIF.textContent = msg;
 
-	if(__notifTimeOfLive__ === null)
-	{
-		// Append it before to compute
-		// `--time-of-live` value.
-		__GLOBAL_NOTIF_CONTAINER__.appendChild( NOTIF );
-		__notifTimeOfLive__ = parseInt( getComputedStyle( NOTIF ).getPropertyValue("--time-of-live") ) * 1000;
-	}
-
-	setTimeout(() =>
+	NOTIF.addEventListener("animationend", () =>
 	{
 		NOTIF.parentNode.removeChild( NOTIF );
-	}, __notifTimeOfLive__);
+	});
 
-	// Append it after to avoid
-	// visual bugs.
 	__GLOBAL_NOTIF_CONTAINER__.appendChild( NOTIF );
 };
