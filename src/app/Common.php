156c8f9e66832316aaefb6cb39d902d590ac472a
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-02-08 17:20:26 -0300

    Update Common.php:
    1 - Remove `include_component` (it is an unnecessary interface).
    2 - Add `include_icon` (echo a SVG, from Lucide collection).

--- src/app/Common.php
+++ src/app/Common.php
@@ -2,8 +2,18 @@
 
 declare(strict_types=1);
 
-function include_component(string $name, array $data = [], array $options = [])
+function include_icon(?string $name = null): void
 {
-	echo view("components/$name", $data, $options);
+	try {
+		$content = file_get_contents("../public/assets/lucide-icons-v0.265.0/$name.svg");
+
+	} catch(\Throwable $ex) {
+		$content = false;
+	}
+
+	if(!$content) // Replacement Character
+		$content = "&#xfffd;"
+
+	echo "<i class='lucide-icon'>$content</i>";
 }
 
