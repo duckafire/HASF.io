4d6f5b3ca8c4181acee431fb87d502fda6869c2b
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-02 10:56:56 -0300

    app/help(html): add function `inc_asset`

--- src/wip/app/Helpers/html_plus_helper.php
+++ src/wip/app/Helpers/html_plus_helper.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Libraries\KeyValue;
+
 if(!enum_exists("SVGIconOrigin"))
 {
 	enum SVGIconOrigin
@@ -80,3 +82,69 @@ if(!function_exists("inc_slt_opt"))
 		echo "<option value='$value'".($isSelected ? "selected='selected'" : "").">$textContent</option>";
 	}
 }
+
+if(!function_exists("inc_asset"))
+{
+	function inc_asset(string $path, string $name, string $ext): void
+	{
+		static $filePath = WRITEPATH."/custom/files-hash.kv";
+		static $data = null;
+
+		$fullFilePath   = "$path/$name.$ext";
+		$clientFilePath = "./assets/$fullFilePath";
+		$serverFilePath = FCPATH."$clientFilePath";
+
+		if($data === null)
+		{
+			if(!create_file($filePath))
+			{
+				echo $clientFilePath;
+				return;
+			}
+
+			$data = kv_decore( $filePath );
+
+			if($data === null)
+			{
+				echo $clientFilePath;
+				return;
+			}
+		}
+
+		if(!isset($data[ $serverFilePath ])
+		{
+			$content = file_get_contents($serverFilePath);
+
+			if($content === false)
+			{
+				log_message("warning", "Impossible get content (file): $serverFilePath";
+				echo $clientFilePath;
+				return;
+			}
+
+			try
+			{
+				$hashedContent = hash("sha256", $content);
+			}
+			catch(ValueError $ex)
+			{
+				log_message("error",   "ValueError (from `hash`): {ex}", ["ex" => $ex]);
+				log_message("warning", "Impossible to hash the content of (file): $serverFilePath");
+				echo $clientFilePath;
+				return;
+			}
+
+			if(file_put_contents($hashedContent) === false)
+			{
+				log_message("warning", "Impossible to update (file): $serverFilePath");
+				echo $clientFilePath;
+				return;
+			}
+
+			$data[ $serverFilePath ] = $hashedContent;
+		}
+
+		echo "$clientFilePath?h={$data[ $serverFilePath ] }";
+	}
+}
+
