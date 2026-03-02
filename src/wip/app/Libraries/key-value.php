decbdd89abb2634469e3c0178f09e45a86e282eb
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-02 08:04:22 -0300

    lib(kv): add KeyValue

---
+++ src/wip/app/Libraries/key-value.php
@@ -0,0 +1,53 @@
+<?php
+
+// This is fastest than file cache of CI4,
+// because it:
+// * reads file once;
+// * increments file (instead rewrite it); and
+// * stores data in as an array (that is updated
+//   together with file).
+
+declare(strict_types=1);
+
+namespace App\Libraries\KeyValue;
+
+const KV_LINE_SEPARATOR = "\n";
+const KV_PAIR_SEPARATOR = "\t";
+
+function kv_format(string $key, string $value): string
+{
+	return $key . KV_PAIR_SEPARATOR . $value . KV_LINE_SEPARATOR;
+}
+
+function kv_decode(string $filePath): ?array
+{
+	if(($content = \file_get_contents($filePath)) === false)
+		return null;
+
+	$data = [];
+
+	foreach(\explode( KV_LINE_SEPARATOR, \trim($content) ) as &$pair)
+		$data[ \strtok($pair, KV_PAIR_SEPARATOR) ] = \strtok(KV_PAIR_SEPARATOR);
+
+	return $data;
+}
+
+function kv_encode(string $destFile, array &$data): bool
+{
+	$content = "";
+
+	foreach($data as $key => &$value)
+		$content .= kv_format($key, $value);
+
+	return (\file_put_contents($destFile, $content) !== false);
+}
+
+function kv_increment(string $destFile, array &$data, string $key, string $value): bool
+{
+	if(\file_put_contents($destFile, kv_format($key, $value), FILE_APPEND) === false)
+		return false;
+
+	$data[ $key ] = $value;
+	return true;
+}
+
