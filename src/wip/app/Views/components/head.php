543da92949ca9b322c5624fd9deeff14ae5a43e9
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-08 14:39:22 -0300

    app(view): Add `<?xml ?>` to all pages

--- src/wip/app/Views/components/head.php
+++ src/wip/app/Views/components/head.php
@@ -1,3 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html class="color-scheme-auto" lang="en-US" dir="ltr">
 <head>
