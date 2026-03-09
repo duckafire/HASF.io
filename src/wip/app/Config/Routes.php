e46a5e3bf2a652d54cb045c790ecbb211afa6266
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-09 18:02:33 -0300

    app(conf): Add new endpoints

--- src/wip/app/Config/Routes.php
+++ src/wip/app/Config/Routes.php
@@ -2,3 +2,9 @@
 
 use CodeIgniter\Router\RouteCollection;
 
+$routes->view("/conv/base",     "conv/base");
+$routes->view("/conv/cipher",   "conv/cipher");
+$routes->view("/conv/encoding", "conv/encoding");
+$routes->view("/conv/hash",     "conv/hash");
+$routes->view("/conv/number",   "conv/number");
+
