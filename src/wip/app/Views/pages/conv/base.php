d20b2ed0638f862955400299a9b6c97756cf46e9
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-09 18:04:01 -0300

    app(view): Add respective views to earlier added endpoints =>
    NOTE: they contain only boilerplates.

---
+++ src/wip/app/Views/pages/conv/base.php
@@ -0,0 +1,13 @@
+<?= view("components/head"); ?>
+<body class="page-content">
+	<?= view("components/nav"); ?>
+
+	<main class="page-main">
+	</main>
+
+	<?= view("components/footer"); ?>
+
+	<script src="./assets/js/tboxes/core.js"></script>
+</body>
+</html>
+
