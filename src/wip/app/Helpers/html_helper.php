4c57ce76c5c8394e231c78790d69ac433145b479
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-11 13:46:15 -0300

    app(help): Refactoring extension of HTML Helper =>
    1 - Remove incorrect namespace.
    2 - Remove enumerator SVGIconOrigin.
    3 - Remove functions: inc_slt_opt; inc_asset.
    4 - Rename functions:
      * inc_icon         -> svg_icon
      * inc_attr_titles  -> attr_titles
      * set_no_translate -> no_translate
      * inc_btn          -> simple_btn
    5 - Declare constants to representate SVG collections.
    6 - Add parameter to `no_translate` to disable class ".notranslate".
    7 - Rework structure of `simple_btn`.
    8 - Remove conditions used to check if a function is already
        declared (it is a exaggeration here).
    9 - Add script guard condition to avoid redeclarations.

--- src/wip/app/Helpers/html_helper.php
+++ src/wip/app/Helpers/html_helper.php
@@ -2,142 +2,66 @@
 
 declare(strict_types=1);
 
-use App\Libraries\KeyValue;
+if(defined("HTML_HELPER_EXTENDED"))
+	return;
 
-if(!enum_exists("SVGIconOrigin"))
+define("HTML_HELPER_EXTENDED", true);
+
+const COLL_LUCIDE    = 0;
+const COLL_BOOTSTRAP = 1;
+
+function svg_icon(string $name, string $collection = COLL_LUCIDE): string
 {
-	enum SVGIconOrigin
+	$dir = match($collection)
 	{
-		case LUCIDE;
-		case BOOTSTRAP;
-	}
+		COLL_LUCIDE    => "lucide-v0.265.0",
+		COLL_BOOTSTRAP => "bootstrap-v1.13.1",
+	};
+
+	$content  = \file_get_contents("../public/assets/images/icons/$dir/$name.svg");
+
+	if($content === false)
+		$content = "&#xfffd;"; // Replacement Character
+
+	return "<i class=\"svg-icon\" aria-hidden=\"true\">$content</i>";
 }
 
-if(!function_exists("inc_icon"))
+function attr_titles(string $text): string
 {
-	function inc_icon(?string $name = null, ?SVGIconOrigin $origin = SVGIconOrigin::LUCIDE): string
-	{
-		$dir = match($origin)
-		{
-			SVGIconOrigin::LUCIDE    => "lucide-v0.265.0",
-			SVGIconOrigin::BOOTSTRAP => "bootstrap-v1.13.1",
-		};
-
-		try {
-			$content = \file_get_contents("../public/assets/images/icons/$dir/$name.svg");
-
-		} catch(\Throwable $ex) {
-			$content = false;
-		}
-
-		if(!$content) // Replacement Character
-			$content = "&#xfffd;";
-
-		return "<i class='svg-icon' aria-hidden='true'>$content</i>";
-	}
+	return "title='$text' aria-label='$text'";
 }
 
-if(!function_exists("inc_attr_titles"))
+function no_translate(bool $notranslateClass = true): string
 {
-	function inc_attr_titles(string $text): string
-	{
-		return "title='$text' aria-label='$text'";
-	}
+	if($notranslateClass)
+		return "translate=\"off\" class=\"notranslate\"";
+
+	return "translate=\"off\"";
 }
 
-if(!function_exists("set_no_translate"))
+function simple_btn(string $classes, string | array $iconData, ?string $title = null, ?string $url = null): string
 {
-	function set_no_translate(?string $classes = ""): string
+	// string $iconData: iconName
+	// array  $iconData: [ iconName, iconCollection ]
+
+	$allTitles = ($titles === null ? "" : attr_titles($title));
+
+	if(\is_string($iconData))
+		$icon = svg_icon($iconData);
+	else
+		$icon = svg_icon($iconData[0], $iconData[1]);
+
+	if($url === null)
 	{
-		return "translate='off' class='notranslate $classes'";
+		$tagName    = "button";
+		$otherAttrs = "";
 	}
-}
-
-if(!function_exists("inc_btn"))
-{
-	function inc_btn(string $classes, string | array $icon, ?string $title = null, ?string $url = null): string
+	else
 	{
-		$allTitles = ($title == null ? "" : "title='$title' aria-label='$title'");
-
-		$icon = "";
-
-		if($icon != null)
-		{
-			if(\gettype($icon) == "string")
-				$icon = inc_icon( $icon );
-			else
-				$icon = inc_icon(icon[0], icon[1]);
-		}
-
-		return ($url == null
-			? "<button          class='$classes' $allTitles>"
-			: "<a role='button' class='$classes' $allTitles href='$url'>") .
-			$icon .
-			($url == null ? "</button>" : "</a>");
+		$tagName    = "a";
+		$otherAttrs = "role=\"button\" href=\"$url\"";
 	}
-}
 
-if(!function_exists("inc_slt_opt"))
-{
-	function inc_slt_opt(string $textContent, string $value, bool $isSelected = false): string
-	{
-		return "<option value='$value'".($isSelected ? "selected='selected'" : "").">$textContent</option>";
-	}
-}
-
-if(!function_exists("inc_asset"))
-{
-	function inc_asset(string $path, string $name, string $ext): string
-	{
-		static $filePath = WRITEPATH."/custom/files-hash.kv";
-		static $data = null;
-
-		$fullFilePath   = "$path/$name.$ext";
-		$clientFilePath = "./assets/$fullFilePath";
-		$serverFilePath = FCPATH."$clientFilePath";
-
-		if($data === null)
-		{
-			if(!create_file($filePath))
-				return $clientFilePath;
-
-			$data = kv_decore( $filePath );
-
-			if($data === null)
-				return $clientFilePath;
-		}
-
-		if(!isset($data[ $serverFilePath ])
-		{
-			$content = file_get_contents($serverFilePath);
-
-			if($content === false)
-			{
-				log_message("warning", "Impossible get content (file): $serverFilePath";
-				return $clientFilePath;
-			}
-
-			try
-			{
-				$hashedContent = hash("sha256", $content);
-			}
-			catch(ValueError $ex)
-			{
-				log_message("error",   "ValueError (from `hash`): {ex}", ["ex" => $ex]);
-				log_message("warning", "Impossible to hash the content of (file): $serverFilePath");
-				return $clientFilePath;
-			}
-
-			if(file_put_contents($hashedContent) === false)
-			{
-				log_message("warning", "Impossible to update (file): $serverFilePath");
-				return $clientFilePath;
-			}
-
-			$data[ $serverFilePath ] = $hashedContent;
-		}
-
-		return "$clientFilePath?h={$data[ $serverFilePath ] }";
-	}
+	return "<$tagName class=\"$classes\" $allTitles $otherAttrs>$icon</$tagName>";
 }
 
