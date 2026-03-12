1b919dc64c294aee6dd2adcaaa0025ed4779bf9a
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-12 19:25:04 -0300

    app(help): Add function `asset` to extension of HTML Helper =>
    It converts `css/foo/bar.css` to `<baseUrl>/assets/foo/bar.<hashCode>.css`,
    by the use of `assets-manifest.json`. If it is not possible to access the
    manifest file, a fallback algorithm will called; it will created the hash
    code in runtime, and will search the file. If it failures too, an exception
    will invoke the error page 500.

--- src/wip/app/Helpers/html_helper.php
+++ src/wip/app/Helpers/html_helper.php
@@ -65,3 +65,60 @@ function simple_btn(string $classes, string | array $iconData, ?string $title = 
 	return "<$tagName class=\"$classes\" $allTitles $otherAttrs>$icon</$tagName>";
 }
 
+function asset(string $relativeFilePath): string
+{
+	static $manifest = null;
+
+	// fallback
+	if($manifest === false)
+	{
+		$absoluteFilePath = \realpath($relativeFilePath);
+
+		if($absoluteFilePath === false)
+		{
+			$errorMessage = "Impossible to get real path of: $relativeFilePath";
+			log_message("critical", $errorMessage);
+			throw new \RuntimeException($errorMessage); // invoke error page 500
+		}
+
+		$filename = \pathinfo($relativeFilePath, PATHINFO_FILENAME);
+		$extname  = \pathinfo($relativeFilePath, PATHINFO_EXTENSION);
+
+		$hashedAbsoluteFilePath = \dirname($absoluteFilePath)."/$filename.*.$extname");
+		$globedFilePath         = \glob($hashedAbsoluteFilePath);
+
+		if($globedFilePath === false || \count($globedFilePath) === 0)
+		{
+			$errorMessage = "Impossible to apply GLOB algorithm to: $hashedAbsoluteFilePath";
+			log_message("critical", $errorMessage);
+			throw new \RuntimeException($errorMessage); // invoke error page 500
+		}
+
+		return base_url("{$globedFilePath[0]}");
+	}
+
+	// start manifest variable
+	if($manifest === null)
+	{
+		$manifest = \file_get_contents(ROOTPATH."/assets-manifest.json");
+
+		if($manifest === false)
+		{
+			log_message("error", "Impossible to read assets-manifest.json");
+			return asset($relativeFilePath);
+		}
+
+		$manifest = \json_decode($manifest);
+
+		if($manifest === null)
+		{
+			$manifest = false; // enable fallback
+			log_message("error", "Impossible to decode assets-manifest.json");
+			return asset($relativeFilePath);
+		}
+	}
+
+	// normal return
+	return base_url($manifest[$relativeFilePath]);
+}
+
