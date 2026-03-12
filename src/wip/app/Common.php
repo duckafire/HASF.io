b94e1dc7b36b2a3f1d0069d284f27a6461123584
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-12 19:02:07 -0300

    app(comm): Remove `create_file` (unnecessary)

--- src/wip/app/Common.php
+++ src/wip/app/Common.php
@@ -2,37 +2,3 @@
 
 declare(strict_types=1);
 
-function create_file(string $filePath): bool
-{
-	$dirPath = \dirname( $filePath );
-
-	if(!\is_dir($dirPath) && !\mkdir($dirPath))
-	{
-		log_message("warning", "Impossible to create (directory): $dirPath");
-		return false;
-	}
-
-	if(!\is_file($filePath))
-	{
-		$file = \fopen( $filePath, "w" );
-
-		if(!$file)
-		{
-			log_message("warning", "Impossible to create (file): $filePath");
-			return false;
-		}
-
-		if(!(\fclose($file))
-		{
-			log_message("warning", "Impossible to close (file): $filePath");
-
-			if(!\unlink($filePath))
-				log_message("warning", "Impossible to delete (file): $filePath");
-
-			return false;
-		}
-	}
-
-	return true;
-}
-
