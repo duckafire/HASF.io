1dc32ca7428dc384b804769042f10c1a071ad380
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-18 16:24:41 -0300

    appl(view): Add boilerplate page to "Home Page"

--- src/wip/app/Config/Routes.php
+++ src/wip/app/Config/Routes.php
@@ -2,11 +2,13 @@
 
 use \CodeIgniter\Router\RouteCollection;
 
+$routes->view("/", "home");
+
+$routes->view("/licenses", "licenses");
+
 $routes->view("/conv/base",     "conv/base");
 $routes->view("/conv/cipher",   "conv/cipher");
 $routes->view("/conv/encoding", "conv/encoding");
 $routes->view("/conv/hash",     "conv/hash");
 $routes->view("/conv/number",   "conv/number");
-
-$routes->view("/licenses", "licenses");
 
