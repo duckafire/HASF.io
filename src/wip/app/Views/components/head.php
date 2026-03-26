60bf8bb9d4244219327716ade1f97948a6cef9f3
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-03-26 17:50:17 -0300

    appl(view): Add "Cache Busting" to favicon through of the use of Query Strings

--- src/wip/app/Views/components/head.php
+++ src/wip/app/Views/components/head.php
@@ -18,7 +18,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	<!--| https://gitlab.com/duckafire/HASF.io ~~~~~~~~ |-->
 	<!--+===============================================+-->
 	<title class="notranslate" translate="no">HASF.io</title>
-	<link rel="icon" href="favicon.svg" type="image/svg+xml"/>
+	<link rel="icon" href="favicon.svg?v=1.0.0" type="image/svg+xml"/>
 
 	<meta property="og:title"       content="HASF.io"/>
 	<meta property="og:image"       content="https://hasf.io/favicon.svg"/>
