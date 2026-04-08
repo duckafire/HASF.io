db90cd48ab67f2dc5a114844510098f5c062eff9
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-04-07 22:40:51 -0300

    appl(conf): Clear routes

--- src/wip/app/Config/Routes.php
+++ src/wip/app/Config/Routes.php
@@ -2,14 +2,3 @@
 
 use \CodeIgniter\Router\RouteCollection;
 
-$routes->view("/",                     "pages/home");
-
-$routes->view("/licenses",             "pages/licenses");
-
-$routes->view("/convert/cipher",       "pages/convert/cipher");
-$routes->view("/convert/code",         "pages/convert/code");
-$routes->view("/convert/encoding",     "pages/convert/encoding");
-$routes->view("/convert/number",       "pages/convert/number");
-
-$routes->view("/create/hash",          "pages/create/hash");
-
