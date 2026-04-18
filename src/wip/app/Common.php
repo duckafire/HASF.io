821048cc2f165f2c73429ab5d40bc0ed8b5eb340
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-04-18 14:38:37 -0300

    appl(comm): Remove constructor of HTTPCode500Exception

--- src/wip/app/Common.php
+++ src/wip/app/Common.php
@@ -5,11 +5,5 @@ declare(strict_types=1);
 \define("DEF_HASH_ALGO", "sha256");
 
 class HTTPCode500Exception extends \Exception
-{
-	function __construct(string $level, string $description)
-	{
-		\log_message($level, $description);
-		parent::__construct();
-	}
-}
+{}
 
