6bb034d7afdd97ceed73544d4ddcb3dbe69c5233
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-04-27 14:41:18 -0300

    cicd(sh__): Refactoring script

--- scripts/install-apache-php.sh
+++ scripts/install-apache-php.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env sh
 
 APACHE_CONFIG_FILE='/etc/apache2/httpd.conf'
+PHP_COMMON_EXT='add php83-common php83-mysqli php83-pgsql php83-sqlite3 php83-gd php83-curl php83-intl php83-mbstring php83-openssl php83-xml php83-zip php83-bcmath php83-soap php83-pcntl php83-posix php83-session php83-ctype php83-dom php83-fileinfo php83-gettext php83-iconv php83-json php83-opcache php83-pdo php83-pdo_mysql php83-pdo_pgsql php83-pdo_sqlite php83-phar php83-simplexml php83-tokenizer php83-xmlreader php83-xmlwriter'
 
-apk update --no-cache --virtual --quiet
-apk add --no-cache --virtual --quiet apache2 php83-apache2 \
-	add php83-common php83-mysqli php83-pgsql php83-sqlite3 php83-gd php83-curl \
-	php83-intl php83-mbstring php83-openssl php83-xml php83-zip php83-bcmath \
-	php83-soap php83-pcntl php83-posix php83-session php83-ctype php83-dom \
-	php83-fileinfo php83-gettext php83-iconv php83-json php83-opcache \
-	php83-pdo php83-pdo_mysql php83-pdo_pgsql php83-pdo_sqlite php83-phar \
-	php83-simplexml php83-tokenizer php83-xmlreader php83-xmlwriter
+apk update --quiet
+apk add --quiet --no-cache apache2 php83-apache2 composer $PHP_COMMON_EXT
 
-cat <<- 'HTTPD_CONF' > "$APACHE_CONFIG_FILE"
+# Just a configuration script:
+cat << 'APACHE_MAIN_CONF_FILE' > "$APACHE_CONFIG_FILE"
 ServerTokens Prod
 ServerRoot /app
 Listen 80
@@ -108,6 +104,5 @@ LoadModule php_module modules/mod_php83.so
     Options None
     Require all granted
 </Directory>
-HTTPD_CONF
+APACHE_MAIN_CONF_FILE
 
-rc-service apache2 start
