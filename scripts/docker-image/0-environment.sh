96e822df9501c5b37da7fe0bb9e2751266ea8722
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-05-08 13:59:59 -0300

    cicd(sh__): Improve SH code

--- scripts/docker-image/0-environment.sh
+++ scripts/docker-image/0-environment.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
 APACHE_CONFIG_FILE='/etc/apache2/httpd.conf'
-PHP_COMMON_EXT='add php83-common php83-mysqli php83-pgsql php83-sqlite3 php83-gd php83-curl php83-intl php83-mbstring php83-openssl php83-xml php83-zip php83-bcmath php83-soap php83-pcntl php83-posix php83-session php83-ctype php83-dom php83-fileinfo php83-gettext php83-iconv php83-json php83-opcache php83-pdo php83-pdo_mysql php83-pdo_pgsql php83-pdo_sqlite php83-phar php83-simplexml php83-tokenizer php83-xmlreader php83-xmlwriter'
+PHP_COMMON_EXT='php83-common php83-mysqli php83-pgsql php83-sqlite3 php83-gd php83-curl php83-intl php83-mbstring php83-openssl php83-xml php83-zip php83-bcmath php83-soap php83-pcntl php83-posix php83-session php83-ctype php83-dom php83-fileinfo php83-gettext php83-iconv php83-json php83-opcache php83-pdo php83-pdo_mysql php83-pdo_pgsql php83-pdo_sqlite php83-phar php83-simplexml php83-tokenizer php83-xmlreader php83-xmlwriter'
 
 apk update --quiet
+echo $PHP_COMNON_EXT
 apk add --quiet --no-cache apache2 php83-apache2 composer $PHP_COMMON_EXT
+
+mkdir --parent "${APACHE_CONFIG_FILE%/*}"
 
 # Just a configuration script:
 cat << 'APACHE_MAIN_CONF_FILE' > "$APACHE_CONFIG_FILE"
--- scripts/docker-image/1-dependences.sh
+++ scripts/docker-image/1-dependences.sh
@@ -4,12 +4,12 @@ ARCHIVE_DIR="/archives"
 APP_DIR="/app/wip"
 SVG_ICONS_DIR="$APP_DIR/public/assets/images/icons"
 
-composer create-project 'codeigniter4/framework:4.6.3' "$APP_DIR"
-
 # Download URL; target directory; and new name
 # to the target directory after move it.
 # (Both they are separated with "#".)
 DEP_LIST="https://github.com/lucide-icons/lucide/archive/refs/tags/v0.265.0.zip#lucide-0.265.0/icons#$SVG_ICONS_DIR/lucide-v0.265.0 https://github.com/twbs/icons/archive/refs/tags/v1.13.1.zip#icons-1.13.1/icons#$SVG_ICONS_DIR/bootstrap-v1.13.1 https://github.com/simple-icons/simple-icons/archive/refs/tags/16.14.0.zip#simple-icons-16.14.0/icon#$SVG_ICONS_DIR/simple-icons-v16.14.0 https://github.com/symfony/yaml/archive/refs/tags/v8.0.6.zip#yaml-8.0.6#$APP_DIR/app/ThirdParty/SymfonyYAML-v8.0.6"
+
+composer create-project 'codeigniter4/framework:4.6.3' "$APP_DIR"
 
 for item in $DEP_LIST
 do
@@ -25,4 +25,3 @@ do
 done
 
 rm -r "$ARCHIVES_DIR"
-
