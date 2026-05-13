59533d3592c83b959ab6f278f5e2cb127ef8f102
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-05-13 01:19:32 -0300

    cicd(sh__): Rework script used to download frameworks, libraries, and similar

--- scripts/docker-image/1-dependences.sh
+++ scripts/docker-image/1-dependences.sh
@@ -1,27 +1,70 @@
 #!/usr/bin/env sh
 
-ARCHIVE_DIR="/archives"
+## Download frameworks, libraries, ..., necessary to run this
+## project.
+
+ARCHIVES_DIR="/app/archives"
+UNPACKED_ARCHIVES_DIR="$ARCHIVES_DIR/unpacked"
 APP_DIR="/app/wip"
 SVG_ICONS_DIR="$APP_DIR/public/assets/images/icons"
 
-# Download URL; target directory; and new name
-# to the target directory after move it.
-# (Both they are separated with "#".)
-DEP_LIST="https://github.com/lucide-icons/lucide/archive/refs/tags/v0.265.0.zip#lucide-0.265.0/icons#$SVG_ICONS_DIR/lucide-v0.265.0 https://github.com/twbs/icons/archive/refs/tags/v1.13.1.zip#icons-1.13.1/icons#$SVG_ICONS_DIR/bootstrap-v1.13.1 https://github.com/simple-icons/simple-icons/archive/refs/tags/16.14.0.zip#simple-icons-16.14.0/icon#$SVG_ICONS_DIR/simple-icons-v16.14.0 https://github.com/symfony/yaml/archive/refs/tags/v8.0.6.zip#yaml-8.0.6#$APP_DIR/app/ThirdParty/SymfonyYAML-v8.0.6"
+# Download URL#Repository target#Destine directory
+DEP_LIST=$(cat << EOF
+https://github.com/lucide-icons/lucide/archive/refs/tags/v0.265.0.zip#lucide-0.265.0/icons#$SVG_ICONS_DIR/lucide-v0.265.0
+https://github.com/twbs/icons/archive/refs/tags/v1.13.1.zip#icons-1.13.1/icons#$SVG_ICONS_DIR/bootstrap-v1.13.1
+https://github.com/simple-icons/simple-icons/archive/refs/tags/16.14.0.zip#simple-icons-16.14.0/icons#$SVG_ICONS_DIR/simple-icons-v16.14.0
+https://github.com/symfony/yaml/archive/refs/tags/v8.0.6.zip#yaml-8.0.6#$APP_DIR/app/ThirdParty/SymfonyYAML-v8.0.6
+EOF
+)
 
-composer create-project 'codeigniter4/framework:4.6.3' "$APP_DIR"
+abort()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+unpackArchive()
+{
+	downloadURL="$1"
+	archivePath="$2"
+
+	archiveFormat="${downloadURL##*.}"
+	archiveFormat="${archiveFormat%\?*}"
+
+	case "$archiveFormat" in
+		"zip") unzip -qd "$UNPACKED_ARCHIVES_DIR" "$archivePath" ;;
+		*)     abort "Unsupported archive format: $archiveFormat" ;;
+	esac
+
+	test $? -ne 0 && abort "Impossible to unpack archive: $archivePath"
+}
+
+#TODO: php extension error
+#composer create-project 'codeigniter4/framework:4.6.3' "$APP_DIR"
+
+mkdir -p "$UNPACKED_ARCHIVES_DIR" "$APP_DIR" "$SVG_ICONS_DIR"
 
 for item in $DEP_LIST
 do
 	middler="${item#*#}"
 
 	downloadURL="${item%%#*}"
-	targetDir="${middler%%#*}"
-	targetDirNewName="${middler#*#}"
+	targetDir="${middler%#*}"
+	destDir="${middler#*#}"
 
-	wget -q -P "$ARCHIVE_DIR" "$downloadURL"
-	unzip -q "$ARCHIVE_DIR/$targetDir"
-	mv "$ARCHIVE_DIR/$targetDir" "$targetDirNewName"
+	archiveName="${downloadURL##*/}"
+	archivePath="$ARCHIVES_DIR/$archiveName"
+	targetDirPath="$UNPACKED_ARCHIVES_DIR/$targetDir"
+
+	wget -qP "$ARCHIVES_DIR" "$downloadURL" 2>/dev/null
+
+	test $? -ne 0 && abort "Impossible to download archive from: $downloadURL"
+
+	unpackArchive "$downloadURL" "$archivePath"
+
+	mkdir -p "${destDir%/*}"
+	mv "$targetDirPath" "$destDir"
 done
 
 rm -r "$ARCHIVES_DIR"
+
