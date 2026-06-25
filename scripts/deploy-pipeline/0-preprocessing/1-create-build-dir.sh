0428089eebe545d5000891312fa9588ab0c5dc0a
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-06-24 22:55:07 -0300

    cicd(sh__): Add code to process macro of main htacces and fallback `index.php`

--- scripts/deploy-pipeline/0-preprocessing/1-create-build-dir.sh
+++ scripts/deploy-pipeline/0-preprocessing/1-create-build-dir.sh
@@ -3,5 +3,18 @@
 set -euo pipefail
 . /dp/export-deploy-env-variables
 
+templatesToProcess="$(cat << EOF
+$HASF_IO_BUILD_ROOT_DIR/../.htaccess
+$HASF_IO_BUILD_ROOT_DIR/../index.php
+EOF
+)"
+
 cp -r "$HASF_IO_BUILD_WIP_DIR" "$HASF_IO_BUILD_ROOT_DIR"
+
+uriRoot="$(echo "${HASF_IO_BUILD_ROOT_DIR#${HASF_IO_WORK_DIR}}" | sed 's/\//\\\//g' | sed 's/^\\\///g')"
+
+for file in $templatesToProcess
+do
+	echo "$(tp "$file" "URI_ROOT=$uriRoot")" > "$file"
+done
 
