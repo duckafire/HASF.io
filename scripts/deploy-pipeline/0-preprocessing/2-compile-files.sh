77e19ce681d3f074f58c5a875a5e13fb15d7c156
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-07-19 12:46:22 -0300

    cicd(sh__): Refactoring deploy pipeline #0#2 =>
    1 - Rename:
      * SCSS_FILES to scssFiles;
      * lf() to catchFiles(); and
      * FILES_TO_REMOVE to sourceFiles.
    2 - Add new constants: MODULES_DIR, SRC_DIRS_LIST, SCSS_PARTIAL_MODULES_DIR,
        TS_MODULES_ROOT_DIR, and TS_MODULES_OUT_DIR.
    3 - Add variables to store "processors" options (<tool-name>_OPTIONS).
    4 - Add condition to created source maps only into development environment.
    5 - Rework function used to catch source files.
    6 - Add condition to delete caught source files if the environment is not of
        development.

--- scripts/deploy-pipeline/0-preprocessing/2-compile-files.sh
+++ scripts/deploy-pipeline/0-preprocessing/2-compile-files.sh
@@ -3,58 +3,107 @@
 set -euo pipefail
 . /dp/export-deploy-env-variables
 
-SCSS_FILES=""
-FILES_TO_REMOVE=""
+MODULES_DIR="$HASF_IO_BUILD_ASSETS_DIR/.modules"
+SRC_DIRS_LIST="$HASF_IO_BUILD_ASSETS_DIR/pages $HASF_IO_BUILD_ASSETS_DIR/components"
 
-# List Files
-lf()
-{
-	dir="$1"
-	ext="$2"
+SCSS_PARTIAL_MODULES_DIR="$MODULES_DIR/styles"
+TS_MODULES_ROOT_DIR="$MODULES_DIR/behaviors/mts"
+TS_MODULES_OUT_DIR="$MODULES_DIR/behaviors/mjs"
 
-	files="$(find "$dir" -name "*.$ext" -type f)"
+SASS_OPTIONS=$(cat <<- EOF
+	--no-color
+	--no-unicode
+	--no-error-css
 
-	# Remove files that are in directories
-	# that must to be ignored.
-	for dir in "fallback images .lib"
-	do
-		# (^|\S*/)dir/\S*
-		files="$(echo "$files" | sed 's/\(^\|\S*\/\)'"$dir"'\/\S*//g')"
-	done
+	--stop-on-error
+	--update
+	--verbose
 
-	echo "$files"
-}
+	--style compressed
+	--load-path "$SCSS_PARTIAL_MODULES_DIR"
+EOF
+)
 
-for file in $(lf "$HASF_IO_BUILD_PUBLIC_DIR" "scss")
-do
-	# Space-separated lists:
-	SCSS_FILES="$SCSS_FILES $file:${file%.*}.css"
-	FILES_TO_REMOVE="$FILES_TO_REMOVE $file"
-done
+TS_OPTIONS=$(cat <<- EOF
+	--target ES6
+	--module NodeNext
+	--moduleResolution NodeNext
+	--lib ES6,DOM,DOM.Iterable
 
-if [ -n "$SCSS_FILES" ]
+	--outDir "$TS_MODULES_OUT_DIR"
+	--rootDir "$TS_MODULES_ROOT_DIR"
+
+	--strict true
+
+	--noImplicitReturns true
+	--noUnusedLocals true
+	--noUnusedParameters true
+	--noImplicitOverride true
+	--noPropertyAccessFromIndexSignature true
+	--noUncheckedIndexedAccess true
+	--exactOptionalPropertyTypes true
+	--forceConsistentCasingInFileNames true
+	--isolatedModules true
+	--esModuleInterop true
+
+	--allowUnreachableCode false
+	--allowUnusedLabels false
+EOF
+)
+
+# (Source map only must be available
+# into development environment.)
+if [ -z "$HASF_IO_PRODUCTION" ]
 then
-	SCSS_PARTIAL_SS_DIR="$HASF_IO_BUILD_ASSETS_DIR/.lib/sass"
-
-	mkdir -p "$SCSS_PARTIAL_SS_DIR"
-
-	SASS_OPTIONS=$(cat <<- EOF
-		--no-color
-		--no-unicode
-		--no-error-css
-
-		--stop-on-error
-		--update
-		--verbose
-
-		--style compressed
-		--load-path "$SCSS_PARTIAL_SS_DIR"
-	EOF
-	)
-
-	# This compressing is minimal.
-	bun x --silent sass $SASS_OPTIONS -- $SCSS_FILES
+	SASS_OPTIONS="$SASS_OPTIONS --source-map"
+	TS_OPTIONS="$TS_OPTIONS --source-map true"
 fi
 
-rm -rf "$FILES_TO_REMOVE" "$(ls -a "$HASF_IO_BUILD_ASSETS_DIR" | grep -E '^\.[^.]+')"
+sourceFiles=""
+
+catchFiles()
+{
+	ext="$1"
+
+	caughtFiles=""
+	outputFiles=""
+
+	for srcDir in $SRC_DIRS_LIST
+	do
+		caughtFiles="$caughtFiles $(find "$srcDir" -name "*.$ext" -type f)"
+		sourceFiles="$sourceFiles $caughtFiles"
+	done
+
+	if [ "$ext" = "scss" ]
+	then
+		for file in $caughtFiles
+		do
+			# input0:output0 ... inputN:outputN
+			outputFiles="$outputFiles $file:${file%.*}.${ext#?}"
+		done
+	fi
+
+	echo "$outputFiles"
+}
+
+scssFiles="$(catchFiles "scss")"
+tsFiles="$(  catchFiles "mts" )"
+
+if [ -n "$scssFiles" ] && [ -d "$SCSS_PARTIAL_MODULES_DIR" ]
+then
+	# (Minimal compression.)
+	bun x --silent sass $SASS_OPTIONS -- $scssFiles
+fi
+
+if [ -n "$tsFiles" ] && [ -d "$TS_MODULES_ROOT_DIR" ]
+then
+	bun x --silent tsc $TS_OPTIONS -- $tsFiles
+fi
+
+# (Source map only must be available
+# into development environment.)
+if [ -n "$HASF_IO_PRODUCTION" ]
+then
+	rm -rf $sourceFiles "$MODULES_DIR"
+fi
 
